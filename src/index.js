f30e2f32c1d93c33d3d1f8913dddd81048c6cafa
Andy Chumak <andrii.chumak@gooddata.com>
2022-10-12 14:04:50 +0200

    Added sorting and enabled colors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import {chart} from '@rawgraphs/rawgraphs-core';
 import {barchart} from '@rawgraphs/rawgraphs-charts';
 
 import backendFactory, {ContextDeferredAuthProvider, redirectToTigerAuthentication} from '@gooddata/sdk-backend-tiger';
-import {MeasureGroupIdentifier, newDimension} from '@gooddata/sdk-model';
+import {
+    MeasureGroupIdentifier,
+    newTwoDimensional,
+    newMeasureSort
+} from '@gooddata/sdk-model';
 import * as cat from './cat';
 
 const gdWorkspace = backendFactory()
@@ -13,14 +17,15 @@ const gdWorkspace = backendFactory()
 window.addEventListener('load', async () => {
     const result = await gdWorkspace.execution()
         .forItems([cat.ProductName, cat.RevenueTop10])
-        .withDimensions(newDimension([cat.ProductName, MeasureGroupIdentifier]))
+        .withSorting(newMeasureSort(cat.RevenueTop10, 'desc'))
+        .withDimensions(...newTwoDimensional([MeasureGroupIdentifier], [cat.ProductName]))
         .execute();
 
     const allData = await result.readAll();
 
-    const headers = allData.headerItems[0][0].map(item => item.attributeHeaderItem.name);
-    const yAxesName = allData.headerItems[0][1][0].measureHeaderItem.name;
-    const userData = allData.data.map((entry, i) => ({
+    const yAxesName = allData.headerItems[0][0][0].measureHeaderItem.name;
+    const headers = allData.headerItems[1][0].map(item => item.attributeHeaderItem.name);
+    const userData = allData.data[0].map((entry, i) => ({
         [yAxesName]: headers[i],
         '$': entry,
     }));
@@ -28,12 +33,16 @@ window.addEventListener('load', async () => {
     const mapping = {
         bars: {value: yAxesName},
         size: {value: '$'},
+        color: {value: '$'},
     };
 
     //instantiating the chart
     const viz = chart(barchart, {
         data: userData,
         mapping,
+        visualOptions: {
+            sortBarsBy: 'original',
+        },
     });
 
     //rendering into the HTML node
